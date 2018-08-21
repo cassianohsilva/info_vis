93ca62901d7e90d1e2b5144788ae4ff9a5b159fc
Cassiano Honorio <honoriocassiano@gmail.com>
2018-08-20 23:41:31 -0300

    Adicionando os arquivos ao carregamento

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var projection = d3.geoNaturalEarth1()
 var path = d3.geoPath(projection);
 var countries = {};
 
-d3.tsv("node_modules/world-atlas/world/110m.tsv").then(function(c) {
+d3.tsv("node_modules/world-atlas/world/50m.tsv").then(function(c) {
 
 	for(let i in c) {
 		countries[c[i]['iso_n3']] = c[i];
@@ -15,7 +15,17 @@ d3.tsv("node_modules/world-atlas/world/110m.tsv").then(function(c) {
 
 }).then(function() {
 
-	d3.json("node_modules/world-atlas/world/110m.json").then(function(world) {
+	Promise.all([
+			d3.csv("data/dataset.csv"),
+			d3.csv("data/types.csv"),
+			d3.json("node_modules/world-atlas/world/110m.json")
+		])
+	.then(function(v) {
+			
+		var data = v[0],
+			types = v[1],
+			world = v[2];
+
 
 		context.append("path")
 			.attr("d", d3.geoPath());
@@ -27,9 +37,52 @@ d3.tsv("node_modules/world-atlas/world/110m.tsv").then(function(c) {
 			return d.id;
 		})
 		.on('mouseover', function(d, n) {
-
+			console.log(d['id'] == parseInt(d['id']));
 		})
-		.attr("d", path);
+		.attr("d", path);		
+
 	});
+
+	// d3.queue()
+	// 	.defer(d3.csv, "data/dataset.csv")
+	// 	.defer(d3.csv, "data/types.csv")
+	// 	.defer(d3.json, "node_modules/world-atlas/world/110m.json")
+	// 	.await(function(error, data, types, world) {
+
+	// 		console.log(error, data, types, world);
+
+	// 		// context.append("path")
+	// 		// 	.attr("d", d3.geoPath());
+
+	// 		// context.selectAll("path")
+	// 		// .data(topojson.feature(world, world.objects.countries).features)
+	// 		// .enter().append("path")
+	// 		// .attr('id', function(d) {
+	// 		// 	return d.id;
+	// 		// })
+	// 		// .on('mouseover', function(d, n) {
+	// 		// 	console.log(d);
+	// 		// })
+	// 		// .attr("d", path);
+	// });
+
+	
+
+	// d3.json("node_modules/world-atlas/world/110m.json").then(function(world) {
+
+	// 	context.append("path")
+	// 		.attr("d", d3.geoPath());
+
+	// 	context.selectAll("path")
+	// 	.data(topojson.feature(world, world.objects.countries).features)
+	// 	.enter().append("path")
+	// 	.attr('id', function(d) {
+	// 		return d.id;
+	// 	})
+	// 	.on('mouseover', function(d, n) {
+
+	// 	})
+	// 	.attr("d", path);
+	// });
 
 });
