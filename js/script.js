869bb64d4b6d4883c06f5ec846a11b4673015e4b
Cassiano Honorio <honoriocassiano@gmail.com>
2018-08-27 13:55:03 -0300

    Adição da quadriculação no mapa

--- js/script.js
+++ js/script.js
@@ -1,88 +1,75 @@
-var context = d3.select("#canvas");
-var projection = d3.geoNaturalEarth1()
-	.scale(150)
-	.translate([370, 220]);
-	// .center([0, 5]);
-
-var path = d3.geoPath(projection);
 var countries = {};
 
-d3.tsv("node_modules/world-atlas/world/50m.tsv").then(function(c) {
+var width = 800,
+    height = 450;
 
-	for(let i in c) {
-		countries[c[i]['iso_n3']] = c[i];
-	}
+var projection = d3.geoNaturalEarth1()
+    .scale(130)
+    .translate([width / 2, height / 2])
+    .precision(.1);
 
-}).then(function() {
+var path = d3.geoPath()
+    .projection(projection);
 
-	Promise.all([
-			d3.csv("data/dataset.csv"),
-			d3.csv("data/types.csv"),
-			d3.json("node_modules/world-atlas/world/110m.json")
-		])
-	.then(function(v) {
-			
-		var data = v[0],
-			types = v[1],
-			world = v[2];
+var graticule = d3.geoGraticule();
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.append("defs").append("path")
+    .datum({type: "Sphere"})
+    .attr("id", "sphere")
+    .attr("d", path);
+
+svg.append("use")
+    .attr("class", "stroke")
+    .attr("xlink:href", "#sphere");
+
+svg.append("use")
+    .attr("class", "fill")
+    .attr("xlink:href", "#sphere");
+
+svg.append("path")
+    .datum(graticule)
+    .attr("class", "graticule")
+    .attr("d", path);
+
+d3.tsv("node_modules/world-atlas/world/50m.tsv").then(function (c) {
+
+    for (let i in c) {
+        countries[c[i]['iso_n3']] = c[i];
+    }
+
+}).then(function () {
+
+    Promise.all([
+        d3.csv("data/dataset.csv"),
+        d3.csv("data/types.csv"),
+        d3.json("node_modules/world-atlas/world/110m.json")
+    ])
+        .then(function (v) {
+
+            var data = v[0],
+                types = v[1],
+                world = v[2];
 
 
-		context.append("path")
-			.attr("d", d3.geoPath());
+            svg.append("path")
+                .attr("d", d3.geoPath())
+                .attr('id', 'map_path');
 
-		context.selectAll("path")
-		.data(topojson.feature(world, world.objects.countries).features)
-		.enter().append("path")
-		.attr('id', function(d) {
-			return d.id;
-		})
-		.on('mouseover', function(d, n) {
-			console.log(d['id'] == parseInt(d['id']));
-		})
-		.attr("d", path);		
+            svg.selectAll("#map_path")
+                .data(topojson.feature(world, world.objects.countries).features)
+                .enter().append("path")
+                .attr('class', 'country')
+                .attr('id', function (d) {
+                    return d.id;
+                })
+                .on('mouseover', function (d, n) {
+                    console.log(countries[d['id']]['sovereignt']);
+                })
+                .attr("d", path);
 
-	});
-
-	// d3.queue()
-	// 	.defer(d3.csv, "data/dataset.csv")
-	// 	.defer(d3.csv, "data/types.csv")
-	// 	.defer(d3.json, "node_modules/world-atlas/world/110m.json")
-	// 	.await(function(error, data, types, world) {
-
-	// 		console.log(error, data, types, world);
-
-	// 		// context.append("path")
-	// 		// 	.attr("d", d3.geoPath());
-
-	// 		// context.selectAll("path")
-	// 		// .data(topojson.feature(world, world.objects.countries).features)
-	// 		// .enter().append("path")
-	// 		// .attr('id', function(d) {
-	// 		// 	return d.id;
-	// 		// })
-	// 		// .on('mouseover', function(d, n) {
-	// 		// 	console.log(d);
-	// 		// })
-	// 		// .attr("d", path);
-	// });
-
-	
-
-	// d3.json("node_modules/world-atlas/world/110m.json").then(function(world) {
-
-	// 	context.append("path")
-	// 		.attr("d", d3.geoPath());
-
-	// 	context.selectAll("path")
-	// 	.data(topojson.feature(world, world.objects.countries).features)
-	// 	.enter().append("path")
-	// 	.attr('id', function(d) {
-	// 		return d.id;
-	// 	})
-	// 	.on('mouseover', function(d, n) {
-
-	// 	})
-	// 	.attr("d", path);
-	// });
-
+        });
 });
